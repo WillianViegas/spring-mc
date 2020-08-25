e4d9e42c119d9de9627973862e9c8669010375bf
Will <willianwed98@gmail.com>
2020-08-25 15:41:32 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/willian/springmc/SpringMcApplication.java
+++ src/main/java/com/willian/springmc/SpringMcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.willian.springmc.domain.Categoria;
+import com.willian.springmc.domain.Produto;
 import com.willian.springmc.repositories.CategoriaRepository;
+import com.willian.springmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringMcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepo;
+	
+	@Autowired
+	private ProdutoRepository produtoRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMcApplication.class, args);
@@ -25,7 +30,19 @@ public class SpringMcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador",2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
