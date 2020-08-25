917f2b1a2931a8ab906a6c4b14c5ee35cedccf26
Will <willianwed98@gmail.com>
2020-08-25 14:08:35 -0300

    Testando classe Categoria

--- src/main/java/com/willian/springmc/resources/CategoriaResource.java
+++ src/main/java/com/willian/springmc/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.willian.springmc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.willian.springmc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest ok!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Escritório");
+		Categoria cat2 = new Categoria(2, "informática");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
