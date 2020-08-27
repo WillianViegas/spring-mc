b1ef573db45371e1e5bf7efb7de277cd5f2d5c3d
Will <willianwed98@gmail.com>
2020-08-27 14:51:00 -0300

    Categoria delete (DELETE)

--- src/main/java/com/willian/springmc/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/com/willian/springmc/resources/exceptions/ResourcesExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.willian.springmc.services.exceptions.DataIntegrityException;
 import com.willian.springmc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,5 +19,11 @@ public class ResourcesExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), Instant.now());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), Instant.now());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/willian/springmc/services/CategoriaService.java
+++ src/main/java/com/willian/springmc/services/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.willian.springmc.domain.Categoria;
 import com.willian.springmc.repositories.CategoriaRepository;
+import com.willian.springmc.services.exceptions.DataIntegrityException;
 import com.willian.springmc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,5 +36,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		findById(obj.getId());
 		return repository.save(obj);
+	}
+	
+	public void deleteById(Integer id) {
+		findById(id);
+		
+		try {
+			repository.deleteById(id);			
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que tenha produtos");
+		}
 	}
 }
