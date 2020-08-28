237edd3480d13f2c8cfa7587ee5db0ed21cd229f
Will <willianwed98@gmail.com>
2020-08-28 13:47:37 -0300

    POST cliente e ClienteNewDTO (configuraçao para salvar cliente e endereco)

--- src/main/java/com/willian/springmc/services/ClienteService.java
+++ src/main/java/com/willian/springmc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.willian.springmc.domain.Cidade;
 import com.willian.springmc.domain.Cliente;
+import com.willian.springmc.domain.Endereco;
+import com.willian.springmc.domain.enums.TipoCliente;
 import com.willian.springmc.dto.ClienteDTO;
+import com.willian.springmc.dto.ClienteNewDTO;
 import com.willian.springmc.repositories.ClienteRepository;
+import com.willian.springmc.repositories.EnderecoRepository;
 import com.willian.springmc.services.exceptions.DataIntegrityException;
 import com.willian.springmc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,8 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repository;
+	@Autowired 
+	private EnderecoRepository endRepository;
 	
 	public List<Cliente> findAll(){
 		List<Cliente> list = repository.findAll();
@@ -30,6 +38,15 @@ public class ClienteService {
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException(id));
+	}
+	
+	//essa annotation permite que tanto o obj cliente quanto o endereco sejam salvos de forma transacional
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		endRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -61,5 +78,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	//converte o objeto Dto como todos os dados (cliente, endereco, telefone e cidade) e instancia eles em suas respectivas classes
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 }
