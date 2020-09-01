17200f47a63e151078de32f3e6638ebdcde2f3cd
Will <willianwed98@gmail.com>
2020-09-01 15:23:48 -0300

    Email HTML

--- src/main/java/com/willian/springmc/services/EmailService.java
+++ src/main/java/com/willian/springmc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.willian.springmc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.willian.springmc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
